dc3007212ddefe64358b8e4b9506db258fe3b003
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2024-08-26 18:45:26 +0800

    feat: EXP-14 | Improve password input

--- lib/widgets/login/custom_text_input.dart
+++ lib/widgets/login/custom_text_input.dart
@@ -18,8 +18,8 @@ class CustomTextFormField extends StatelessWidget {
     return TextFormField(
       controller: controller,
       validator: validator,
+      obscureText: labelText == 'Password' ? true : false,
       decoration: InputDecoration(
-        // labelText: labelText,
         label: Text(labelText),
         border: const OutlineInputBorder(
           borderSide: BorderSide(color: Colors.grey),
